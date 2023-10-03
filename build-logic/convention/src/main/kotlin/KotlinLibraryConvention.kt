192946e0b9a163d76111a5bd27f450e4303a5c28
John-Tuesday <10697715+John-Tuesday@users.noreply.github.com>
2023-10-03 07:53:41 -0500

    Set the build jvmargs to gradle suggested alternatives and ignoreDisabledTargets=true
    increased heap size and other things
    optimized imports in KotlinLibraryConvention

--- build-logic/convention/src/main/kotlin/KotlinLibraryConvention.kt
+++ build-logic/convention/src/main/kotlin/KotlinLibraryConvention.kt
@@ -4,7 +4,6 @@ import org.calamarfederal.physical.measurement.configureNative
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.configure
-import org.gradle.kotlin.dsl.dependencies
 import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
 
 class KotlinLibraryConvention : Plugin<Project> {
