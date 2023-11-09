eed66d676888d60a1ef2d89483225f8e52b07db2
John-Tuesday <10697715+John-Tuesday@users.noreply.github.com>
2023-11-09 17:54:28 -0600

    Add documentation

--- measure/measure-test/src/commonMain/kotlin/io/github/john/tuesday/measurement/test/AssertHelpers.kt
+++ measure/measure-test/src/commonMain/kotlin/io/github/john/tuesday/measurement/test/AssertHelpers.kt
@@ -6,6 +6,9 @@ import kotlin.test.assertTrue
 
 private const val DefaultTolerance = 0.000_000_001
 
+/**
+ * Asserts the difference between [actual] and [expected] is within [absoluteTolerance] with optional [message]
+ */
 public fun Length.Companion.assertEquals(
     expected: Length,
     actual: Length,
@@ -13,6 +16,9 @@ public fun Length.Companion.assertEquals(
     message: String? = null
 ): Unit = assertEquals(expected.inMeters(), actual.inMeters(), absoluteTolerance = absoluteTolerance, message = message)
 
+/**
+ * Asserts the difference between [actual] and [expected] is within [absoluteTolerance] with optional [message]
+ */
 public fun Length.Companion.assertEquals(
     expected: Length,
     actual: Length,
@@ -20,6 +26,9 @@ public fun Length.Companion.assertEquals(
     message: String? = null
 ): Unit = assertTrue((expected - actual).absoluteValue <= absoluteTolerance.absoluteValue, message = message)
 
+/**
+ * Asserts the difference between [actual] and [expected] is within [absoluteTolerance] with optional [message]
+ */
 public fun Mass.Companion.assertEquals(
     expected: Mass,
     actual: Mass,
@@ -27,12 +36,19 @@ public fun Mass.Companion.assertEquals(
     message: String? = null
 ): Unit = assertEquals(expected.inGrams(), actual.inGrams(), absoluteTolerance = absoluteTolerance, message = message)
 
+/**
+ * Asserts the difference between [actual] and [expected] is within [absoluteTolerance] with optional [message]
+ */
 public fun Mass.Companion.assertEquals(
     expected: Mass,
     actual: Mass,
     absoluteTolerance: Mass,
     message: String? = null
 ): Unit = assertTrue((expected - actual).absoluteValue <= absoluteTolerance.absoluteValue, message = message)
+
+/**
+ * Asserts the difference between [actual] and [expected] is within [absoluteTolerance] with optional [message]
+ */
 public fun Volume.Companion.assertEquals(
     expected: Volume,
     actual: Volume,
@@ -40,6 +56,9 @@ public fun Volume.Companion.assertEquals(
     message: String? = null
 ): Unit = assertEquals(expected.inLiters(), actual.inLiters(), absoluteTolerance = absoluteTolerance, message = message)
 
+/**
+ * Asserts the difference between [actual] and [expected] is within [absoluteTolerance] with optional [message]
+ */
 public fun Volume.Companion.assertEquals(
     expected: Volume,
     actual: Volume,
@@ -47,6 +66,9 @@ public fun Volume.Companion.assertEquals(
     message: String? = null
 ): Unit = assertTrue((expected - actual).absoluteValue <= absoluteTolerance.absoluteValue, message = message)
 
+/**
+ * Asserts the difference between [actual] and [expected] is within [absoluteTolerance] with optional [message]
+ */
 public fun Energy.Companion.assertEquals(
     expected: Energy,
     actual: Energy,
@@ -54,6 +76,9 @@ public fun Energy.Companion.assertEquals(
     message: String? = null
 ): Unit = assertEquals(expected.inJoules(), actual.inJoules(), absoluteTolerance = absoluteTolerance, message = message)
 
+/**
+ * Asserts the difference between [actual] and [expected] is within [absoluteTolerance] with optional [message]
+ */
 public fun Energy.Companion.assertEquals(
     expected: Energy,
     actual: Energy,
