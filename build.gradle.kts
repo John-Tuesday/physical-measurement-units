2b6441755bf8caa7d20288863299cf3e1bcbc298
John-Tuesday <10697715+John-Tuesday@users.noreply.github.com>
2023-11-10 09:06:12 -0600

    Add in-house Maven Publish Assist plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,7 @@ import io.github.john.tuesday.measurement.MavenVersion
 plugins {
     alias(libs.plugins.kotlin.multiplatform) apply false
     alias(libs.plugins.dokka) apply true
+    alias(libs.plugins.maven.assist) apply false
     id("measure.kotlin.library") apply false
 }
 
