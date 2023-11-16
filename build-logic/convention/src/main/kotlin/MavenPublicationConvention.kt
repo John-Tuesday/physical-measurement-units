0904255b5a30237067d217aaa4c228f60b241cc2
John-Tuesday <10697715+John-Tuesday@users.noreply.github.com>
2023-11-16 12:29:35 -0600

    Add GitHubPackages maven repo

--- build-logic/convention/src/main/kotlin/MavenPublicationConvention.kt
+++ build-logic/convention/src/main/kotlin/MavenPublicationConvention.kt
@@ -4,9 +4,9 @@ import io.github.john.tuesday.plugins.MavenPublishAssistPlugin
 import io.github.john.tuesday.plugins.publishing.model.LicensePreset
 import io.github.john.tuesday.plugins.publishing.model.MavenRepository
 import io.github.john.tuesday.plugins.publishing.model.license
+import io.github.john.tuesday.plugins.publishing.model.maven
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.api.credentials.PasswordCredentials
 import org.gradle.api.publish.PublishingExtension
 import org.gradle.api.publish.maven.MavenPublication
 import org.gradle.kotlin.dsl.*
@@ -15,29 +15,34 @@ class MavenConvention : Plugin<Project> {
     override fun apply(target: Project) {
         with(target) {
             pluginManager.apply(MavenPublishAssistPlugin::class)
-            pluginManager.withPlugin("maven-publish") {
-                val publishing = extensions.getByType<PublishingExtension>()
-                publishing.repositories {
-                    maven {
-                        setUrl(MavenRepository.SonatypeStaging.url)
-                        credentials(PasswordCredentials::class)
-                    }
-                }
-                publishing.publications.withType<MavenPublication>().configureEach {
-                    pom {
-                        name = "Physical Measurement Units"
-                        description = "Simple Kotlin Multiplatform library which supplies Mass, Volume, and Length"
-                        url = GitUrl
+            val publishing = extensions.getByType<PublishingExtension>()
+            publishing.repositories {
+                maven(
+                    repo = MavenRepository.SonatypeStaging,
+                    providers = providers,
+                )
+                maven(
+                    repo = MavenRepository.GitHubPackage(
+                        owner = "john-tuesday",
+                        repository = "physical-measurement-units"
+                    ),
+                    providers = providers,
+                )
+            }
+            publishing.publications.withType<MavenPublication>().configureEach {
+                pom {
+                    name = "Physical Measurement Units"
+                    description = "Simple Kotlin Multiplatform library which supplies Mass, Volume, and Length"
+                    url = GitUrl
 
-                        scm {
-                            url = GitUrl
-                        }
-                        licenses {
-                            license(LicensePreset.MIT)
-                        }
-                        developers {
-                            johnTuesday()
-                        }
+                    scm {
+                        url = GitUrl
+                    }
+                    licenses {
+                        license(LicensePreset.MIT)
+                    }
+                    developers {
+                        johnTuesday()
                     }
                 }
             }
