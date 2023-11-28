e04be85569c5b72cbde8f97939fdb0f83ddc6148
John-Tuesday <10697715+John-Tuesday@users.noreply.github.com>
2023-11-28 11:10:22 -0600

    Add clearer error when property / env variable cannot be found

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,8 +6,14 @@ pluginManagement {
             name = "GitHubPackages"
             url = uri("https://maven.pkg.github.com/john-tuesday/gradle-convention-plugins")
             credentials {
-                username = providers.gradleProperty("gpr.user").orNull ?: System.getenv("USERNAME")
-                password = providers.gradleProperty("gpr.key").orNull ?: System.getenv("TOKEN")
+                username = providers
+                    .gradleProperty("gpr.user")
+                    .orElse(providers.environmentVariable("USERNAME"))
+                    .orNull ?: error("Expected to find property 'gpr.user' or environment variable 'USERNAME' but found nothing")
+                password = providers
+                    .gradleProperty("gpr.key")
+                    .orElse(providers.environmentVariable("TOKEN"))
+                    .orNull ?: error("Expected to find property 'gpr.key' or environment variable 'TOKEN' but found nothing")
             }
         }
     }
