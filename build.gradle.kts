3ffd2faf65a1d929127bc161e0ddc27776da3ad4
John-Tuesday <10697715+John-Tuesday@users.noreply.github.com>
2023-12-13 17:35:50 -0600

    Update convention to 0.1.0-alpha05 and use dokka versioning convention

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,7 @@ import io.github.john.tuesday.measurement.MavenVersion
 plugins {
     alias(libs.plugins.kotlin.multiplatform) apply false
     alias(libs.plugins.dokka.convention.html) apply true
+    alias(libs.plugins.dokka.convention.versioning) apply true
     alias(libs.plugins.maven.assist) apply false
     id("measure.kotlin.library.multiplatform") apply false
     id("measure.maven.publish") apply false
