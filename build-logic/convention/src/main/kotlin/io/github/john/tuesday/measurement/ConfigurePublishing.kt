ed009b225ee204f9c66cbb4d0f4af49e2e2d5626
John-Tuesday <10697715+John-Tuesday@users.noreply.github.com>
2023-12-13 23:20:02 -0600

    Generate documentation and release 2.0.0-alpha04

--- build-logic/convention/src/main/kotlin/io/github/john/tuesday/measurement/ConfigurePublishing.kt
+++ build-logic/convention/src/main/kotlin/io/github/john/tuesday/measurement/ConfigurePublishing.kt
@@ -1,5 +1,5 @@
 package io.github.john.tuesday.measurement
 
 const val MavenGroupId = "io.github.john-tuesday"
-const val MavenVersion = "2.0.0-alpha03"
+const val MavenVersion = "2.0.0-alpha04"
 const val GitUrl = "https://github.com/John-Tuesday/physical-measurement-units"
